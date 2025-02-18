ce3f40252be46c77b717588bda5913678ede3334
Anugerah Rachmadani <anugerahrama04@gmail.com>
2025-02-18 10:39:52 +0700

    jobsheet 2 - praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '
+    Nama: Anugerah Rachmadani S.
+    <br>
+    Nim: 244107023012    
+    ';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya ' . $name;
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
 });
